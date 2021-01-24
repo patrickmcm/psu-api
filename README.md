ae98eee5ca3f9f0bf2001b74c42b2f26fff68fe4
peanut1120 <happyguy564h@gmail.com>
2021-01-24 10:11:07 +0000

    upload to github

---
+++ README.md
@@ -0,0 +1,20 @@
+## Node.js PSU API obfuscation
+
+# Setup
+
+First go to nodejs.org and install the current version. 
+After that, install all the npm packages:
+
+```js
+npm install
+```
+
+Then config the app.js with your API key, open up app.js in a text editor, and on the 3rd line input your API key.
+
+Finally put your script in the putscripthere.txt file then run
+
+```js
+npm start
+```
+
+You'll get an output.txt file in the same directory.
---
+++ app.js
@@ -0,0 +1,37 @@
+const psu = require("psu-obf-api");
+const fs = require("fs")
+const apiKey = "apikeyhere"
+
+function read(file, callback) {
+    fs.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data);
+    });
+}
+
+const fileTobeObfuscated = read('putscripthere.txt', function(data) {
+
+// settings you want here
+let options = {
+    "DisableSuperOperators": false,
+    "MaximumSecurityEnabled": false,
+    "ControlFlowObfuscation": true,
+    "ConstantEncryption": false,
+    "EncryptAllStrings": false,
+    "DisableAllMacros": false,
+    "EnhancedOutput": false,
+    "EnhancedConstantEncryption": false,
+    "CompressedOutput": false,
+    "PremiumFormat": true,
+    "ByteCodeMode": "Default"
+
+}
+let scriptObfuscation = psu.obfuscate(apiKey,data,options)
+.then(ret => {
+    fs.writeFile('output.txt', ret.data, function(err) {
+        console.log("Obfuscation done, saved to output file.")
+      });
+})
+});
